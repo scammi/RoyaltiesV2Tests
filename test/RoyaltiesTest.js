1330c43cb38d6655a4569a232bd941d52e946bb2
Your Name <scammi@gmail.com>
2022-03-31 13:00:32 -0300

    For each

--- test/RoyaltiesTest.js
+++ test/RoyaltiesTest.js
@@ -5,6 +5,8 @@ const { ethers } = require("hardhat");
 
 const kovanAddresses = require('@charged-particles/protocol-subgraph/networks/kovan');
 const prontonBAbi =  require('@charged-particles/protocol-subgraph/abis/ProtonB');
+const chargedSettings =  require('@charged-particles/protocol-subgraph/abis/ChargedSettings');
+
 const chargeStateAbi =  require('@charged-particles/protocol-subgraph/abis/ChargedState');
 
 describe("V2", function () {
@@ -16,45 +18,62 @@ describe("V2", function () {
   const myWallet = walletMnemonic.connect(provider)
 
   // init contracts
-  const protonContract = new ethers.Contract(kovanAddresses.proton.address, prontonBAbi, provider);
+  const protronBAdress = kovanAddresses.protonB.address;
+  const protonContract = new ethers.Contract(protronBAdress, prontonBAbi, provider);
+  const chargeSetting = new ethers.Contract(kovanAddresses.chargedSettings.address, chargedSettings, provider);
   // const chargeStateContract = new ethers.Contract(kovanAddresses.chargedState.address, chargeStateAbi);
 
-  it("Sets royalties from 0", async function () {
-    // init addresses
-    const recipientWallet = ethers.Wallet.createRandom();
-    const recipientWalletAddress = recipientWallet.address;
+  let protonId = 7;
+
+  it("Creates new particles with 0% royalties", async() => {
+    if(Boolean(protonId)) {
+      protonId = await protonContract.connect(myWallet).callStatic.createBasicProton(
+        myWallet.address,
+        myWallet.address,
+        TOKEN_URI 
+      )
+      console.log('New token:',protonId.toNumber(), '\n');
+    
+      const newProtonTrx = await protonContract.connect(myWallet).createBasicProton(
+        myWallet.address, 
+        myWallet.address,
+        TOKEN_URI,
+      );
+      await newProtonTrx.wait();
+      console.log('Proton created \n');
+    
+      const royaltiesOnStart = await protonContract.connect(myWallet).getCreatorRoyaltiesPct(protonId);
+      const annuitiesOnStart = await chargeSetting.connect(myWallet).getCreatorAnnuities(protronBAdress, protonId);
+      
+      expect(royaltiesOnStart).to.equal(ethers.BigNumber.from("0"));
+      expect(annuitiesOnStart?.value).to.equal(ethers.BigNumber.from("0"));
+    }  
+
+    const setNewAnnuityPercentage = await chargeSetting.connect(myWallet).setCreatorAnnuities(protronBAdress, protonId, myWallet.address, ethers.BigNumber.from("400"));
+    await setNewAnnuityPercentage.wait()
+
+    console.log(setNewAnnuityPercentage)
+
+    const newAnnuityPercentage = await chargeSetting.connect(myWallet).getCreatorAnnuities(protronBAdress, protonId);
+    console.log(newAnnuityPercentage);
+
+    // expect(newAnnuityPercentage?.value).to.equal(ethers.BigNumber.from(500));
+
+    // console.log(newAnnuityPercentage?.value)
+  })
+
+  // for( let royaltyPct = 100; royaltyPct <= 1000; royaltyPct += 100) {
+  //   it(`Modify proton royalties to ${royaltyPct == 0 ? 0 : royaltyPct/10}%`, async function() {
+
+  //     const setNewRoyaltyPercentage = await protonContract.connect(myWallet).setRoyaltiesPct(protonId, royaltyPct);
+  //     await setNewRoyaltyPercentage.wait()
   
-    // create
-    const newProtonId = await protonContract.connect(myWallet).callStatic.createBasicProton(
-      myWallet.address,
-      myWallet.address,
-      TOKEN_URI 
-    )
-    console.log('New token:',newProtonId.toNumber(), '\n');
-
-    const newProtonTrx = await protonContract.connect(myWallet).createBasicProton(
-      myWallet.address, 
-      myWallet.address,
-      TOKEN_URI,
-    );
-    await newProtonTrx.wait();
-    console.log('Proton created \n');
-
-    const royaltiesOnStart = await protonContract.connect(myWallet).getCreatorRoyaltiesPct(newProtonId);
-    // const receiptRoyaltiesOnStart = await royaltiesOnStart.wait();
-    expect(royaltiesOnStart).to.equal(ethers.BigNumber.from("0"));
-
-    for( let pc = 100; pc <= 1000; pc += 100) {
-      console.log('PERCETAGE ',pc)
-      const setNewRoyaltyPercentage = await protonContract.connect(myWallet).setRoyaltiesPct(newProtonId, pc);
-      await setNewRoyaltyPercentage.wait()
-  
-      const newSetRoyaltyPercentage = await protonContract.connect(myWallet).getCreatorRoyaltiesPct(newProtonId);
-      expect(newSetRoyaltyPercentage).to.equal(ethers.BigNumber.from(newSetRoyaltyPercentage));
-    }
-    // console.log(royaltiesOnFive, receiptRoyaltiesOnFive);
-    // console.log(getRoyaltiesOnFive);
+  //     const newSetRoyaltyPercentage = await protonContract.connect(myWallet).getCreatorRoyaltiesPct(protonId);
+  //     expect(newSetRoyaltyPercentage).to.equal(ethers.BigNumber.from(newSetRoyaltyPercentage));
+  //   })
+  // }
 
 
-  });
+
+
 });
